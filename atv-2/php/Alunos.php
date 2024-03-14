d989ab12818bad91e4fb9a3d52c247945fd21575
Matusalém de Sousa <124395590+Matusal3m@users.noreply.github.com>
2024-03-14 16:24:42 -0300

    c

--- atv-2/php/Alunos.php
+++ atv-2/php/Alunos.php
@@ -1,10 +1,22 @@
 <?php
 
 $notasAlunos = [[]];
+$medias = [];
 
 for( $i = 0; $i < 10; $i++ ) {
   for( $j = 0; $j < 4; $j++ ) {
-    $notasAlunos[$i][$j] = readline("Coloque a " , $i , "ª nota do " , $j , "º aluno(a)");
+    echo "Coloque a " , $j + 1 , "ª nota do " , $i + 1 , "º aluno(a): ";
+    $notasAlunos[$i][$j] = readline("");
   }
   echo "\n";
+}
+
+for( $i = 0; $i < 10; $i++ ) {
+  $medias[$i] = ($notasAlunos[$i][0] + $notasAlunos[$i][1] + $notasAlunos[$i][2] + $notasAlunos[$i][3]) / 4;
+}
+
+echo "As respectivas médias são: ";
+
+foreach ($medias as $media) {
+  echo $media , " ";
 }
